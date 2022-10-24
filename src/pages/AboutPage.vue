acf9f91d8b93a87b0ba62aad48fec53949bf9272
marciaalegria <ytasegundo@gmail.com>
2022-10-23 22:28:30 -0300

    paleta scss

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -2,8 +2,8 @@
     <q-page padding>
       <div class="q-pa-md row justify-center">
         <div style="width: 100%; max-width: 400px">
-          <q-chat-message :text="['hey, how are you?']" sent name="me" />
-          <q-chat-message :text="['doing fine, how r you?']" name="cocodibuja" />
+          <q-chat-message :text="['hey, vas a la entrevista?']" sent name="me" />
+          <q-chat-message :text="['sí, deséame éxito?']" name="JuanCri" />
         </div>
       </div>
     </q-page>
